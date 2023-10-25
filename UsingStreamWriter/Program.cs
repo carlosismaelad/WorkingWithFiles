48cfe60811f155c882d923decdfc2aae47c26ed5
carlosismaelad <carlosismaelad@outlook.com>
2023-10-24 23:58:59 -0300

    Conhecendo o StreamWriter.

---
+++ UsingStreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace UsingStreamWriter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string sourcePath = @"c:\text\file1.txt";
+            string targetPath = @"c:\text\file2.txt";
+
+            // Vamos transformar todo o conteúdo de file1 em
+            // uppercase para o file2
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+        }
+    }
+}
