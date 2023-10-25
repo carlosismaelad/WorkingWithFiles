26f30f5e2c3accf99f87f3e60d015616ba08f8d6
carlosismaelad <carlosismaelad@outlook.com>
2023-10-24 22:29:03 -0300

    Conhecendo o FileStream e o StreamReader

---
+++ UsingFileStreamAndStreamReader/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace UsingFileStreamAndStreamReader
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string path = @"c:\text\file1.txt";
+
+            // Recurso externo não gerenciado pelo CLR
+            // Uma conversa com o sistema operacional
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                string line = sr.ReadLine();
+                Console.Write(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+            finally
+            {
+                // Fechando as streams
+                // Se for diferente de nulo (aberto)
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
